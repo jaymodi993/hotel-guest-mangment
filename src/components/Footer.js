861a1cb60fd090d1de181eb2a984bcc9c67495e4
DELL <pdip11062004@gmail.com>
2024-07-02 18:14:46 +0530

    code successfully run

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,6 @@ import React from "react";
 function Footer() {
   return (
     <>
-
-=======
       <div classname="container-fluid px-4">
         <div classname="d-flex align-items-center justify-content-between small">
           <div classname="text-muted">Copyright © Your Website 2023</div>
