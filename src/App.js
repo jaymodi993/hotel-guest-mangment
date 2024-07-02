9e90cf7630e357c7b059be8c5e100362229e3760
Jay Modi <jaymodi993@gmail.com>
2024-07-02 17:35:41 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Sidepan from './components/Sidepan'
 
 function App() {
   return (
     <>
       <Header />
       <Footer />
+      <Sidepan/>
     </>
   );
 }
