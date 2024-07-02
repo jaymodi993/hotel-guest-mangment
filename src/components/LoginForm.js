5e89a610da9aec4ccd0e2c506775afd72310e4af
Jay Modi <jaymodi993@gmail.com>
2024-07-02 21:04:35 +0530

    fi

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,59 +1,58 @@
 import React from "react";
+import App from '../App';
 
 function LoginForm() {
   return (
-    <div className="login-form">
+    <div classname="login-form">
       <form>
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-lg-5">
-              <div class="card shadow-lg border-0 rounded-lg mt-5">
-                <div class="card-header">
-                  <h3 class="text-center font-weight-light my-4">Login</h3>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-5">
+              <div className="card shadow-lg border-0 rounded-lg mt-5">
+                <div className="card-header">
+                  <h3 className="text-center font-weight-light my-4">Login</h3>
                 </div>
-                <div class="card-body">
-                  <form>
-                    <div class="form-floating mb-3">
-                      <input
-                        class="form-control"
-                        id="UserName"
-                        type="UserName"
-                        placeholder="UserName"
-                      />
-                      <label for="UserName">UserName</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                      <input
-                        class="form-control"
-                        id="inputPassword"
-                        type="password"
-                        placeholder="Password"
-                      />
-                      <label for="inputPassword">Password</label>
-                    </div>
-                    <div class="form-check mb-3">
-                      <input
-                        class="form-check-input"
-                        id="inputRememberPassword"
-                        type="checkbox"
-                        value=""
-                      />
-                      <label
-                        class="form-check-label"
-                        for="inputRememberPassword"
-                      >
-                        Remember Password
-                      </label>
-                    </div>
-                    <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                      <a class="small" href="password.html">
-                        Forgot Password?
-                      </a>
-                      <a class="btn btn-primary" href="index.html">
-                        Login
-                      </a>
-                    </div>
-                  </form>
+                <div className="card-body">
+                  <div className="form-floating mb-3">
+                    <input
+                      className="form-control"
+                      id="UserName"
+                      type="UserName"
+                      placeholder="UserName"
+                    />
+                    <label htmlFor="UserName">UserName</label>
+                  </div>
+                  <div className="form-floating mb-3">
+                    <input
+                      className="form-control"
+                      id="inputPassword"
+                      type="password"
+                      placeholder="Password"
+                    />
+                    <label htmlFor="inputPassword">Password</label>
+                  </div>
+                  <div className="form-check mb-3">
+                    <input
+                      className="form-check-input"
+                      id="inputRememberPassword"
+                      type="checkbox"
+                      defaultValue
+                    />
+                    <label
+                      className="form-check-label"
+                      htmlFor="inputRememberPassword"
+                    >
+                      Remember Password
+                    </label>
+                  </div>
+                  <div className="d-flex align-items-center justify-content-between mt-4 mb-0">
+                    <a className="small" href="password.html">
+                      Forgot Password?
+                    </a>
+                    <a className="btn btn-primary" href= <App/> >
+                      Login
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
