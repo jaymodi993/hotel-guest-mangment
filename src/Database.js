70acd1e85c88b5bff13743e5b33ca6be8f068e1a
DELL <pdip11062004@gmail.com>
2024-07-04 13:55:47 +0530

    change

--- src/Database.js
+++ src/Database.js
@@ -12,4 +12,16 @@ export const roomMaster = [
   { roomnumber: 6 },
   { roomnumber: 7 },
   { roomnumber: 8 },
+  { roomnumber: 9 },
+  { roomnumber: 10 },
+  { roomnumber: 11 },
+  { roomnumber: 12 },
+  { roomnumber: 13 },
+  { roomnumber: 14 },
+  { roomnumber: 15 },
+  { roomnumber: 16 },
+  { roomnumber: 17 },
+  { roomnumber: 18 },
+  { roomnumber: 19 },
+  { roomnumber: 20 }
 ];
